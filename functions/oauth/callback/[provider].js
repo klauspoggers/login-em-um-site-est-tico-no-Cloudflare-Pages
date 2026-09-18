ae8bb10e6d78974657fd282432337a9baee3fcbf
klauspoggers <klaus.silva2006@gmail.com>
2026-09-18 16:32:27 -0300

    Update [provider].js

--- functions/oauth/callback/[provider].js
+++ functions/oauth/callback/[provider].js
@@ -69,7 +69,10 @@ export async function onRequestGet(context) {
         body: tokenBody.toString()
     });
     
-    if (!tokenResponse.ok) return new Response('Erro na troca de código por token.', { status: 400, headers: { 'Cache-Control': 'no-store' } });
+    if (!tokenResponse.ok) {
+    const detalhesErro = await tokenResponse.text();
+    return new Response('Erro do Google: ' + detalhesErro, { status: 400, headers: { 'Cache-Control': 'no-store' } });
+    }
     const tokenData = await tokenResponse.json();
 
     let issuer, subject, email, displayName;
